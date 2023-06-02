4c83729732cd7263c6249de84528a328f02a29c7
Benjamin Hoch <130229375+hochben@users.noreply.github.com>
2023-06-02 19:25:49 +0200

    Changed Sidebar and cleaned code in templates

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    // Collapse sidebar toggle
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
-    
-    // Custom Scrollbar
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-   });
-
-   $('#sidebarCollapse').on('click', function () {
-    // open or close navbar
-    $('#sidebar').toggleClass('active');
-    // close dropdowns
-    $('.collapse.in').toggleClass('in');
-    });
-
-   //
-
+    //
 });
